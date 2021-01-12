4f329b96d627e4a5047613006fc992c9d56b1659
Pranjal Timsina <pranjal.timsina@live.com>
2021-01-13 00:12:34 +0545

    barely working stuff

---
+++ index.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+
+const { prefix, token } = require('./config.json');
+
+const client = new Discord.Client();
+
+
+client.on('message', message => {
+	if (message.content.startsWith(`${prefix}ping`)) {
+		message.channel.send('Pong.');
+	}
+	else if (message.content.startsWith(`${prefix}beep`)) {
+		message.channel.send('Boop.');
+	}
+	else if (message.content === `${prefix}server`) {
+		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+	}
+	else if (message.content === `${prefix}user-info`) {
+		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+	}
+});
+
+client.login(token);
+
+client.once('ready', readyDiscord);
+
+function readyDiscord() {
+	console.log('Logged in');
+}
