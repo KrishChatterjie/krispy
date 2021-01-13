21479df5be1bcd27018499437a10c1a3be7b0864
Pranjal Timsina <pranjal.timsina@live.com>
2021-01-13 20:21:11 +0545

    added commnds.js files

--- index.js
+++ index.js
@@ -38,27 +38,25 @@ client.on('message', message => {
 		console.log(message.mentions.users.first());
 	}
 
-	if (command == 'assign') {
+	if (command === 'assign') {
 		client.commands.get('assign').execute(message, args);
 	}
 	else if (command === 'ping') {
 		client.commands.get('ping').execute(message, args);
-		
 	}
-	else if (command == 'show') {
-		message.channel.send('Show task.');
-		//history
+	else if (command === 'show') {
+		client.commands.get('show').execute(message, args);
 	}
-	else if (command == 'done') {
-		message.channel.send('Task completed.');
+	else if (command === 'done') {
+		client.commands.get('done').execute(message, args);
 	}
-	else if (command == 'progress') {
-		message.channel.send('Task being done.');
+	else if (command === 'progress') {
+		client.commands.get('progress').execute(message, args);
 	}
-	else if (command == 'server') {
+	else if (command === 'server') {
 		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
 	}
-	else if (command == 'user-info') {
+	else if (command === 'user-info') {
 		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
 	}
       
