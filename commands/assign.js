b53e103c55019cdea416312ca7499984863b7984
iamyajat <myajat21@gmail.com>
2021-01-15 20:56:26 +0530

    Modified assign.js

--- commands/assign.js
+++ commands/assign.js
@@ -4,20 +4,49 @@ module.exports = {
 	usage: "@role/@user [task name]",
 	description: "Assigns task to role/user",
 	execute(message, args) {
-		if (message.mentions.users.first() != null) {
-			message.mentions.users
-				.first()
-				.send(args.join(" ") + `\n\n**Assigned by:** @` + message.author.tag, {
-					split: true,
-				})
-				.then(() => {
-					if (message.channel.type !== "dm") {
-						message.channel.send(`I\'ve assigned and sent <@` + message.mentions.users.first() + `> the task.`);
-					}
-				})
-				.catch(() => message.reply("Something is wrong :("));
-		} else {
-			message.channel.send("Tell me who to assign it to, you idiot :)");
+
+		let users = message.mentions.users;
+		let assignees_id = new Array();
+		for (const user of users) {
+			let user_copy = user.slice();
+			assignees_id.push(user_copy.shift());
 		}
+
+		let taskName = "";
+		let lenArgs = args.length;
+		for(let i=lenArgs-1;i>=0;i--){
+			let word = args[i];
+			if(!word.startsWith('<')){
+				taskName = word+" "+taskName;
+			}
+			else {
+				taskName = taskName.trim();
+				break;
+			}
+		}
+
+		let assignerId = message.author.id;
+
+		console.log(assignees_id);
+		console.log("-----------------");
+		console.log(assignerId);
+		console.log("-----------------");
+		console.log(taskName);
+
+
+		message.mentions.users.map(user => {
+
+			user.send(taskName + `\n\n**Assigned by:** <@` + message.author+`>`, {
+				split: true,
+			})
+			.then(() => {
+				if (message.channel.type !== "dm") {
+					message.channel.send(`I\'ve assigned and sent <@` + user + `> the task.`);
+				}
+			})
+			.catch(() => message.reply(`Something went wrong on <@` + user + `>'s end :(`));
+
+		})
+
 	},
 };
