cd2fa8e98c7792b7f624ab0e9e98122d31ba8623
Sharanya Mukherjee <59661067+sharanya02@users.noreply.github.com>
2021-01-25 00:03:21 +0530

    Delete relations.js

--- models/relations.js
+++
@@ -1,8 +0,0 @@
-const user = require('./user');
-const server = require('./server');
-const task = require('./task');
-
-
-user.belongsToMany(task, { through: 'user_tasks' });
-task.belongsToMany(user, { through: 'user_task' });
-user.hasMany(server,{foreignKey: 'UserID'});
