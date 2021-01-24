d17ca9f5f876552bec658d0bb00a0dfaeb6c4a22
Sharanya Mukherjee <59661067+sharanya02@users.noreply.github.com>
2021-01-25 00:21:23 +0530

    linted ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,10 @@
+
 module.exports = {
-	name: 'ping',
-	description: 'Checks if the bot is online.',
-	aliases: ["check", "online","checkonline"],
-    usage: "",
-	execute(message, args) {
-		message.channel.send('Pong! I\'m here!!');
-	},
-};
+  name: 'ping',
+  description: 'Checks if the bot is online.',
+  aliases: ['check', 'online', 'checkonline'],
+  usage: '',
+  execute (message, args) {
+    message.channel.send('Pong! I\'m here!!')
+  }
+}
