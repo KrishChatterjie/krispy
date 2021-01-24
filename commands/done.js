46b1d12c3ac709fec62441e00d606e14305869e1
Sharanya Mukherjee <59661067+sharanya02@users.noreply.github.com>
2021-01-25 00:18:06 +0530

    Update done.js

--- commands/done.js
+++ commands/done.js
@@ -1,10 +1,10 @@
 module.exports = {
-    name: 'done',
-	aliases: ["donetask", "finished", "finishedtask"],
-	usage: "[task name] [__%]",
-	description: 'Completes the task',
-	execute(message, args) {
-		message.channel.send('Done... but what?');
-		var user = message.author; //this stores the author's ID (Ig y'all need that... ryt?)
-	},
-};
+  name: 'done',
+  aliases: ['donetask', 'finished', 'finishedtask'],
+  usage: '[task name] [__%]',
+  description: 'Completes the task',
+  execute (message, args) {
+    message.channel.send('Done... but what?')
+    const user = message.author // this stores the author's ID (Ig y'all need that... ryt?)
+  }
+}
