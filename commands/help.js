949375092fa7e38e3e476683d938197277498afa
Sharanya Mukherjee <59661067+sharanya02@users.noreply.github.com>
2021-01-25 00:19:35 +0530

    linted all folders

--- commands/help.js
+++ commands/help.js
@@ -1,40 +1,37 @@
 module.exports = {
-    name: "help",
-    description: "List all of the commands or info about a specific command.",
-    aliases: ["commands"],
-    usage: "[command name]",
-    execute(message, args) {
-        const {
-            commands
-        } = message.client;
-        const data = [];
+  name: 'help',
+  description: 'List all of the commands or info about a specific command.',
+  aliases: ['commands'],
+  usage: '[command name]',
+  execute (message, args) {
+    const {
+      commands
+    } = message.client
+    const data = []
 
-        if (!args.length) {
-            data.push(`Here's a list of all commands:`);
-            data.push(
-                `\`` + commands.map((command) => command.name).join("`, `") + `\``
-            );
-            data.push(
-                `\nYou can send \`~help [command name]\` to get info on a specific command!`
-            );
-        } else {
-            if (!commands.has(args[0])) {
-                return message.reply(`That's not a valid command, sorry.`);
-            }
-            const command = commands.get(args[0]);
-            
-            data.push(`**Name:** ${command.name}`);
-            if (command.description)
-                data.push(`**Description:** ${command.description}`);
-            if (command.aliases)
-                data.push(`**Aliases:** ${command.aliases.join(", ")}`);
-            if (command.usage)
-                data.push(`**Usage:** \`~${command.name} ${command.usage}\``);
-        }
-        message.channel.send(data.join("\n"));
+    if (!args.length) {
+      data.push('Here\'s a list of all commands:')
+      data.push(
+        '`' + commands.map((command) => command.name).join('`, `') + '`'
+      )
+      data.push(
+        '\nYou can send `~help [command name]` to get info on a specific command!'
+      )
+    } else {
+      if (!commands.has(args[0])) {
+        return message.reply('That\'s not a valid command, sorry.')
+      }
+      const command = commands.get(args[0])
 
-        //To send msg via DMs
-        /*
+      data.push(`**Name:** ${command.name}`)
+      if (command.description) { data.push(`**Description:** ${command.description}`) }
+      if (command.aliases) { data.push(`**Aliases:** ${command.aliases.join(', ')}`) }
+      if (command.usage) { data.push(`**Usage:** \`~${command.name} ${command.usage}\``) }
+    }
+    message.channel.send(data.join('\n'))
+
+    // To send msg via DMs
+    /*
         message.author.send(data.join("\n"), { split: true })
             .then(() => {
                 if (message.channel.type !== "dm") {
@@ -43,5 +40,5 @@ module.exports = {
             })
             .catch(() => message.reply("It seems I can't send help to you by DM."));
         */
-    },
-};
+  }
+}
