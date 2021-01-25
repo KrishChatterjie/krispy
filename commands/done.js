9deb97e6ef09bd9153f8a60329473a4b73882d22
iamyajat <myajat21@gmail.com>
2021-01-25 18:01:59 +0530

    done.js Updated

--- commands/done.js
+++ commands/done.js
@@ -1,22 +1,25 @@
 module.exports = {
-  name: 'done',
-  aliases: ['donetask', 'finished', 'finishedtask'],
-  usage: '[task name] [__%]',
-  description: 'Completes the task',
-  execute (message, args) {
-    // message.channel.send('Done... but what?');
-    const user = message.author;
-    const user_id = user.id;
-    let prog = args.pop();
-    try{
-      prog = int(prog.slice(0,prog.length-1));
-    }
-    catch {
-      message.channel.send("Dumb or what?");
-      return
-    }
-    
-    const taskName = args.join(' ');
-    console.log(taskName, prog);
-  }
+	name: 'done',
+	aliases: ['donetask', 'finished', 'finishedtask'],
+	usage: '[task name] [__%]',
+	description: 'Completes the task',
+	execute(message, args) {
+		// message.channel.send('Done... but what?');
+		console.log(args);
+		const user = message.author;
+		const user_id = user.id;
+		let prog = args.pop();
+		console.log(prog);
+		try {
+			prog = int(prog.slice(0, prog.length - 1));
+			console.log('try', typeof (prog));
+		} catch {
+			console.log('catch', typeof (prog));
+			message.channel.send("Dumb or what? Changes made");
+			return
+		}
+		console.log('Out');
+		const taskName = args.join(' ');
+		console.log(taskName, prog);
+	}
 }
