458c51e435eb778ff6e0d7d394532151c3425b89
iamyajat <myajat21@gmail.com>
2021-01-25 17:33:26 +0530

    done.js Updated

--- commands/assign.js
+++ commands/assign.js
@@ -5,93 +5,103 @@ const uuid4 = require('uuid4')
 const getInfo = require('../getrolesusers')
 
 module.exports = {
-  name: 'assign',
-  aliases: ['give', 'givetask', 'assigntask'],
-  usage: '@role/@user [task name]',
-  description: 'Assigns task to role/user',
-  async execute (message, args) {
-    // if role is mentioned, get a list of all users with that role from the database store it as an array
-    // the elements in the array will be converted to the users object
-    // if user is mentioned after that, append that user to the existing array we get from db
-    // repeat the code below for all the users
-console.log('hello')
-    const users = message.mentions.users
+	name: 'assign',
+	aliases: ['give', 'givetask', 'assigntask'],
+	usage: '@role/@user [task name]',
+	description: 'Assigns task to role/user',
+	async execute(message, args) {
+		// if role is mentioned, get a list of all users with that role from the database store it as an array
+		// the elements in the array will be converted to the users object
+		// if user is mentioned after that, append that user to the existing array we get from db
+		// repeat the code below for all the users
+		// console.log('hello')
+		const users = message.mentions.users
 
-    const assignees_id = new Array()
-    for (const user of users) {
-      const user_copy = user.slice()
-      assignees_id.push(user_copy.shift())
-    }
+		const assignees_id = new Array()
+		for (const user of users) {
+			const user_copy = user.slice()
+			assignees_id.push(user_copy.shift())
+		}
 
-    // working
-    const roles = message.mentions.roles
-    async function asyncFunction () {
-      for (const role of roles) {
-        const x = await getInfo.getUsers(message.guild, role)
-        message.channel.send(x)
-      }
-    }
-    //
+		// working
+		const roles = message.mentions.roles
+		async function asyncFunction() {
+			for (const role of roles) {
+				const x = await getInfo.getUsers(message.guild, role)
+				message.channel.send(x)
+			}
+		}
+		//
 
-    let taskName = ''
-    const lenArgs = args.length
-    for (let i = lenArgs - 1; i >= 0; i--) {
-      const word = args[i]
-      if (!word.startsWith('<')) {
-        taskName = word + ' ' + taskName
-      } else {
-        taskName = taskName.trim()
-        break
-      }
-    }
+		let taskName = ''
+		const lenArgs = args.length
+		for (let i = lenArgs - 1; i >= 0; i--) {
+			const word = args[i]
+			if (!word.startsWith('<')) {
+				taskName = word + ' ' + taskName
+			} else {
+				taskName = taskName.trim()
+				break
+			}
+		}
 
-    const assignerId = message.author.id
+		const assignerId = message.author.id
 
-    console.log(assignees_id);
-    console.log("-----------------");
-    console.log(assignerId);
-    console.log("-----------------");
-    console.log(taskName);
-    async function addtask (assignees_id, assignerId, taskName) {
-      try {
-			  const exist = await user.findOne({ where: { discordId:  assignees_id , ServerId: message.guild.id } })
-			  const exist1 = await user.findOne({ where: { discordId: assignerId, ServerId: message.guild.id } })
-			  const taskk = {
-          taskId: uuid4(),
-          taskname: taskName,
-          assignee: exist.UserId,
-          assigner: exist1.UserId
-			  }
-			  const newTask = await task.create(taskk)
-			  return {
-          error: false,
-          message: 'Task Successfully Created',
-          code: 201,
-          task: newTask
-			  }
-      } catch (err) {
-			  return {
-          error: true,
-          message: 'An Error Occurred' + err,
-          code: 500
-			  }
-      }
-    }
+		// console.log(assignees_id);
+		// console.log("-----------------");
+		// console.log(assignerId);
+		// console.log("-----------------");
+		// console.log(taskName);
+		async function addtask(assignees_id, assignerId, taskName) {
+			try {
+				const exist = await user.findOne({
+					where: {
+						discordId: assignees_id,
+						ServerId: message.guild.id
+					}
+				})
+				const exist1 = await user.findOne({
+					where: {
+						discordId: assignerId,
+						ServerId: message.guild.id
+					}
+				})
+				const taskk = {
+					taskId: uuid4(),
+					taskname: taskName,
+					assignee: exist.UserId,
+					assigner: exist1.UserId
+				}
+				const newTask = await task.create(taskk)
+				return {
+					error: false,
+					message: 'Task Successfully Created',
+					code: 201,
+					task: newTask
+				}
+			} catch (err) {
+				return {
+					error: true,
+					message: 'An Error Occurred' + err,
+					code: 500
+				}
+			}
+		}
 
-     const a= await addtask(assignees_id, message.author.id, taskName)
-     console.log(a)
-    
-    
-    message.mentions.users.map(user => {
-      user.send('**Task:** ' + taskName + '\n**Assigned by:** <@' + message.author + '>', {
-        split: true
-      })
-        .then(() => {
-          if (message.channel.type !== 'dm') {
-            message.channel.send('I\'ve assigned and sent <@' + user + '> the task.')
-          }
-        })
-        .catch(() => message.reply('Something went wrong on <@' + user + '>\'s end :('))
-    })
-  }
+		const a = await addtask(assignees_id, message.author.id, taskName)
+		console.log(a)
+
+
+		message.mentions.users.map(user => {
+			user.send('**Task:** ' + taskName + '\n**Assigned by:** <@' + message.author + '>', {
+					split: true
+				})
+				.then(() => {
+					if (message.channel.type !== 'dm') {
+						message.channel.send('I\'ve assigned and sent <@' + user + '> the task.')
+					}
+				})
+				.catch(() => message.reply('Something went wrong on <@' + user + '>\'s end :('))
+		})
+	}
 }
--- commands/done.js
+++ commands/done.js
@@ -4,7 +4,19 @@ module.exports = {
   usage: '[task name] [__%]',
   description: 'Completes the task',
   execute (message, args) {
-    message.channel.send('Done... but what?')
-    const user = message.author // this stores the author's ID (Ig y'all need that... ryt?)
+    // message.channel.send('Done... but what?');
+    const user = message.author;
+    const user_id = user.id;
+    let prog = args.pop();
+    try{
+      prog = int(prog.slice(0,prog.length-1));
+    }
+    catch {
+      message.channel.send("Dumb or what?");
+      return
+    }
+    
+    const taskName = args.join(' ');
+    console.log(taskName, prog);
   }
 }
