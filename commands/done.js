601dd3e29d3b9724edca94a7791b7f5c61266e2c
iamyajat <myajat21@gmail.com>
2021-01-25 18:48:54 +0530

    Updated done.js

--- commands/done.js
+++ commands/done.js
@@ -4,22 +4,64 @@ module.exports = {
 	usage: '[task name] [__%]',
 	description: 'Completes the task',
 	execute(message, args) {
-		// message.channel.send('Done... but what?');
-		console.log(args);
+		
 		const user = message.author;
 		const user_id = user.id;
+		
 		let prog = args.pop();
-		console.log(prog);
+		
 		try {
-			prog = int(prog.slice(0, prog.length - 1));
-			console.log('try', typeof (prog));
+			prog = parseInt((prog.slice(0, prog.length - 1)));
 		} catch {
-			console.log('catch', typeof (prog));
-			message.channel.send("Dumb or what? Changes made");
+			message.channel.send("Dumb or what? Do it properly.");
 			return
 		}
-		console.log('Out');
+		
 		const taskName = args.join(' ');
-		console.log(taskName, prog);
+
+    //db
+    
+		async function addtask(assignees_id, assignerId, taskName) {
+			try {
+				const exist = await user.findOne({
+					where: {
+						discordId: assignees_id,
+						ServerId: message.guild.id
+					}
+				})
+				const exist1 = await user.findOne({
+					where: {
+						discordId: assignerId,
+						ServerId: message.guild.id
+					}
+				})
+				const taskk = {
+					taskId: uuid4(),
+					taskname: taskName,
+					assignee: exist.UserId,
+					assigner: exist1.UserId
+				}
+				const newTask = await task.create(taskk)
+				return {
+					error: false,
+					message: 'Task Successfully Created',
+					code: 201,
+					task: newTask
+				}
+			} catch (err) {
+				return {
+					error: true,
+					message: 'An Error Occurred' + err,
+					code: 500
+				}
+			}
+		}
+
+		// const a = await addtask(assignees_id, message.author.id, taskName)
+		const a = await donetask(user_id, taskName, prog)
+		console.log(a)
+    
+    //db
+		
 	}
 }
